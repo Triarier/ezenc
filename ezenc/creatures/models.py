06e9932afedef27b5c9f4cbd015eb4dcc1624906
Florian 'Solo' Stertz <florian.stertz@gmail.com>
2024-03-12 20:37:03 +0100

    radar charts

--- ezenc/creatures/models.py
+++ ezenc/creatures/models.py
@@ -9,6 +9,13 @@ class Creature(models.Model):
     hit_dice = models.TextField(null=True)
     armor_class = models.TextField()
     speed = models.TextField()
+    strength = models.IntegerField(default=10)
+    dexterity = models.IntegerField(default=10)
+    constitution = models.IntegerField(default=10)
+    intelligence = models.IntegerField(default=10)
+    wisdom = models.IntegerField(default=10)
+    charisma = models.IntegerField(default=10)
+    challenge_rating = models.FloatField(default=1)
 
     def __str__(self):
         return f"Name: {self.name}\nDescription: {self.description}\nHit Points: {self.hit_points}\nHit Dice: {self.hit_dice}\nArmor Class: {self.armor_class}\nSpeed: {self.speed}"
--- ezenc/creatures/views.py
+++ ezenc/creatures/views.py
@@ -2,11 +2,39 @@ from django.shortcuts import render
 
 from creatures.models import Creature
 import sys
+import plotly.express as px
+import pandas as pd
 
 
 def index(request):
     print(f"Current directory: {sys.path}")
     creature_list = Creature.objects.all()
+    for creature in creature_list:
+        df = pd.DataFrame(
+            dict(
+                r=[
+                    creature.strength,
+                    creature.dexterity,
+                    creature.constitution,
+                    creature.intelligence,
+                    creature.wisdom,
+                    creature.charisma,
+                ],
+                theta=[
+                    "Strength",
+                    "Dexterity",
+                    "Constitution",
+                    "Intelligence",
+                    "Wisdom",
+                    "Charisma",
+                ],
+            )
+        )
+        fig = px.line_polar(df, r="r", theta="theta", line_close=True)
+        fig.update_layout(polar_radialaxis_showticklabels=False)
+        creature.radar = fig.to_html(
+            full_html=False, default_height=300, default_width=300
+        )
     context = {
         "creature_list": creature_list,
     }
